6af322aceb65a8aee96a5898d6950b43a08221ef
a.osetckiy <a.osetckiy@extracode.net>
2020-06-21 12:29:57 +0300

    Added test on get courses of model

--- src/test/java/net/os/goodcourses/controller/CourseControllerTest.java
+++ src/test/java/net/os/goodcourses/controller/CourseControllerTest.java
@@ -10,11 +10,18 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Collection;
+import static junit.framework.TestCase.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebAppConfiguration
@@ -35,8 +42,21 @@ public class CourseControllerTest {
     }
 
     @Test
-    public void testMainPageCourses() throws Exception {
+    public void testRedirectOnMainPageCourses() throws Exception {
         this.mockMvc.perform(get("/courses"))
                 .andExpect(forwardedUrl("/WEB-INF/JSP/courses.jsp"));
+    }
+
+    @Test
+    public void testOnGetCoursesOfModel() throws Exception {
+        ResultActions resultActions = this.mockMvc.perform(get("/courses"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        ModelAndView coursesModelAndView = resultActions.andReturn().getModelAndView();
+        assert coursesModelAndView != null;
+        ModelMap coursesModelMap = coursesModelAndView.getModelMap();
+        Collection<Object> courses = (Collection<Object>) coursesModelMap.get("courses");
+        assertTrue(courses.size() > 0);
     }
 }
